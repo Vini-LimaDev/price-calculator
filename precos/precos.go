c9aca6e921905c3d7498dd9aa600d22ce1b93254
Vinicius Lima <viniciusalmeidalima@icloud.com>
2025-10-21 09:35:07 -0300

    add: arquivo para centralizar lógica

--- precos/precos.go
+++ precos/precos.go
@@ -1,7 +1,31 @@
 package precos
 
+import (
+	"fmt"
+)
+
 type CalculadoraDePrecos struct {
 	Taxas          float64
 	Precos         []float64
 	TaxasComPrecos map[string]float64
+}
+
+func (c *CalculadoraDePrecos) Calcular() {
+	resultado := make(map[string]float64)
+
+	for _, preco := range c.Precos {
+		resultado[fmt.Sprintf("%.2f", preco)] = preco * (1 + c.Taxas)
+	}
+
+	fmt.Println("Taxa:", c.Taxas)
+	fmt.Println("Preços com taxa:", resultado)
+}
+
+func NovoCalculadoraDePrecos(taxas float64) *CalculadoraDePrecos {
+	// Pegar os precos apartir de um arquivo
+
+	return &CalculadoraDePrecos{
+		Precos: []float64{10, 20, 30},
+		Taxas:  taxas,
+	}
 }
